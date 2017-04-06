a07251c65ae7be814cdce2f2fb484d088d76b616
marcochristmann <Christm5@students.uni-marburg.de>
2017-04-06 03:22:33 +0200

    source:externalize strings

--- src/animal/Parrot.java
+++ src/animal/Parrot.java
@@ -15,13 +15,13 @@ public class Parrot extends Animal {
 	private void makeDyingNoise() {
 		System.out.println(new DyingNoise() {
 			public String noise() {
-				return "raaaw......";
+				return Messages.getString("Parrot.0"); //$NON-NLS-1$
 			}
 		}.noise());		
 	}
 	
 	public void babble() {
-		String s = "Hello";
+		String s = Messages.getString("Parrot.1"); //$NON-NLS-1$
 		System.out.println(s);
 	}
 
